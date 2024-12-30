4708abb08d073a75aabe9dd66c22bbf6da39a3b6
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2024-12-30 13:33:00 +0300

    add all transfers to table

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,4 +1,4 @@
-import { ERC20, Account, Token, HistoricalAccount } from "generated";
+import { ERC20, Account, Token, HistoricalAccount, Transfer } from "generated";
 import { getOrCreateToken } from "./viem/Contract";
 import { walletCache } from "./WalletCache";
 import { priceFetcher } from "./PriceFetcher";
@@ -42,6 +42,23 @@ ERC20.Transfer.handlerWithLoader({
       console.log(e);
     }
 
+    const transferObject: Transfer = {
+      id: event.transaction.hash + event.logIndex.toString(),
+      from: event.params.from.toLowerCase(),
+      to: event.params.to.toLowerCase(),
+      token: event.srcAddress.toLowerCase(),
+      value: event.params.value,
+      timestamp: BigInt(event.block.timestamp),
+      blockNumber: BigInt(event.block.number),
+      transactionHash: event.transaction.hash,
+      logIndex: event.logIndex,
+    };
+
+    if (event.params.from === event.params.to) {
+      context.Transfer.set(transferObject);
+      return;
+    }
+
     if (senderAccount === undefined && claveAddresses.has(event.params.from.toLowerCase())) {
       // create the account
       let accountObject: Account = {
@@ -57,6 +74,7 @@ ERC20.Transfer.handlerWithLoader({
         id: accountObject.id + event.block.timestamp.toString(),
         timestamp: BigInt(event.block.timestamp),
       });
+      context.Transfer.set(transferObject);
     } else if (claveAddresses.has(event.params.from.toLowerCase())) {
       // subtract the balance from the existing users balance
       let accountObject: Account = {
@@ -72,6 +90,7 @@ ERC20.Transfer.handlerWithLoader({
         id: accountObject.id + event.block.timestamp.toString(),
         timestamp: BigInt(event.block.timestamp),
       });
+      context.Transfer.set(transferObject);
     }
 
     if (receiverAccount === undefined && claveAddresses.has(event.params.to.toLowerCase())) {
@@ -89,6 +108,7 @@ ERC20.Transfer.handlerWithLoader({
         id: accountObject.id + event.block.timestamp.toString(),
         timestamp: BigInt(event.block.timestamp),
       });
+      context.Transfer.set(transferObject);
     } else if (claveAddresses.has(event.params.to.toLowerCase())) {
       // update existing account
       let accountObject: Account = {
@@ -104,6 +124,7 @@ ERC20.Transfer.handlerWithLoader({
         id: accountObject.id + event.block.timestamp.toString(),
         timestamp: BigInt(event.block.timestamp),
       });
+      context.Transfer.set(transferObject);
     }
   },
   wildcard: true,
